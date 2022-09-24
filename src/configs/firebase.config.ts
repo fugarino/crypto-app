05c954b4e2d1f821afa459cfbd5e6ef58a36b61e
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-09-24 06:50:47 -0400

    build: firebase config setup

--- src/configs/firebase.config.ts
+++ src/configs/firebase.config.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCizRHN4Ptu8_Airl_zw_SmSKIhXS4jASY",
-  authDomain: "crypto-82a86.firebaseapp.com",
-  projectId: "crypto-82a86",
-  storageBucket: "crypto-82a86.appspot.com",
-  messagingSenderId: "1089909587643",
-  appId: "1:1089909587643:web:37ff423213ca8f5e34b39e",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
